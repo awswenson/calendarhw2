8fce5430488a7e51bcfec039b47b3f1997f28647
Alex Swenson <awswenson@wisc.edu>
2016-03-04 15:09:19 -0600

    Change up layout of CalendarActivity and created a basic skeleton for CalendarActivity.

--- app/src/main/java/com/example/alexswenson/calendarhw2/CalendarActivity.java
+++ app/src/main/java/com/example/alexswenson/calendarhw2/CalendarActivity.java
@@ -2,28 +2,53 @@ package com.example.alexswenson.calendarhw2;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CalendarView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class CalendarActivity extends AppCompatActivity {
+
+    private CalendarView calendarView;
+    private ListView eventsListView;
+
+    private ArrayList<Event> eventsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        calendarView = (CalendarView) findViewById(R.id.calendar_calendarView);
+        eventsListView = (ListView) findViewById(R.id.events_listView);
+
+        eventsList = new ArrayList<>();
+
+        // TODO Set up an adapter for the eventsList
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                setEventsListForDate(year, month, dayOfMonth);
+            }
+        });
+
+        // Set the action of the 'Add' button
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                // TODO Make another activity to create an event and store it in the database. Then
+                // refresh the eventsList to display the newly added event
             }
         });
     }
@@ -48,5 +73,10 @@ public class CalendarActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void setEventsListForDate(int year, int month, int dayOfMonth) {
+        // TODO Get the events for the given date from the database and display them in the listView
+        // using the eventsList and adapter.
     }
 }
---
+++ app/src/main/java/com/example/alexswenson/calendarhw2/Event.java
@@ -0,0 +1,9 @@
+package com.example.alexswenson.calendarhw2;
+
+/**
+ * Created by Alex Swenson on 3/4/16.
+ */
+public class Event {
+
+    // TODO Include title of event, date and time
+}
