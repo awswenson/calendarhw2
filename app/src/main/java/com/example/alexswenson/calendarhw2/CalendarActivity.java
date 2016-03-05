d7849872b69722438bfff777f4ca059f78fe9c04
Alex Swenson <swensona@charter.net>
2016-03-05 14:05:16 -0600

    Added the ability to query the database for events and display them in the eventsListView

--- app/src/main/java/com/example/alexswenson/calendarhw2/CalendarActivity.java
+++ app/src/main/java/com/example/alexswenson/calendarhw2/CalendarActivity.java
@@ -1,5 +1,6 @@
 package com.example.alexswenson.calendarhw2;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -7,10 +8,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.CalendarView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import de.greenrobot.dao.QueryBuilder;
 
 public class CalendarActivity extends AppCompatActivity {
 
@@ -18,6 +25,13 @@ public class CalendarActivity extends AppCompatActivity {
     private ListView eventsListView;
 
     private ArrayList<Event> eventsList;
+    private ArrayAdapter eventsListAdapter;
+
+    private DaoMaster.DevOpenHelper calendarDBHelper;
+    private SQLiteDatabase calendarDB;
+    private DaoMaster daoMaster;
+    private DaoSession daoSession;
+    private EventDao eventDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,13 +46,21 @@ public class CalendarActivity extends AppCompatActivity {
 
         eventsList = new ArrayList<>();
 
-        // TODO Set up an adapter for the eventsList
+        // Setup the eventsListAdapter
+        eventsListAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, eventsList);
+        eventsListView.setAdapter(eventsListAdapter);
+
+        // initialise the database
+        initDatabase();
+
+        // Populate the eventsList for the date selected on the calendar view
+        setEventsListForDate(new Date(calendarView.getDate()));
 
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
 
             @Override
             public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-                setEventsListForDate(year, month, dayOfMonth);
+                setEventsListForDate(new Date(view.getDate()));
             }
         });
 
@@ -75,8 +97,77 @@ public class CalendarActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void setEventsListForDate(int year, int month, int dayOfMonth) {
-        // TODO Get the events for the given date from the database and display them in the listView
-        // using the eventsList and adapter.
+    public void setEventsListForDate(Date date) {
+
+        Calendar today = Calendar.getInstance();
+        today.setTime(date);
+        today.set(Calendar.HOUR_OF_DAY, 0);
+        today.set(Calendar.MINUTE, 0);
+        today.set(Calendar.SECOND, 0);
+        today.set(Calendar.MILLISECOND, 0);
+
+        Calendar tomorrow = Calendar.getInstance();
+        tomorrow.setTime(date);
+        tomorrow.add(Calendar.DATE, 1);
+        tomorrow.set(Calendar.HOUR_OF_DAY, 0);
+        tomorrow.set(Calendar.MINUTE, 0);
+        tomorrow.set(Calendar.SECOND, 0);
+        tomorrow.set(Calendar.MILLISECOND, 0);
+
+        // Get list of Guest objects in database using QueryBuilder.
+        // If list is null, then database tables were created for first time,
+        // so we call "closeReopenDatabase()" to reopen the database.
+        QueryBuilder queryBuilder = eventDao.queryBuilder();
+        List<Event> eventListFromDB = queryBuilder.where(EventDao.Properties.Date.between(today.getTime(), tomorrow.getTime())).list();
+
+        if (eventListFromDB == null) {
+            closeReopenDatabase();
+            eventListFromDB = queryBuilder.where(EventDao.Properties.Date.between(today.getTime(), tomorrow.getTime())).list();
+        }
+
+        eventsList.clear();
+        eventsList.addAll(eventListFromDB);
+
+        eventsListAdapter.notifyDataSetChanged();
+    }
+
+    private void initDatabase() {
+        calendarDBHelper = new DaoMaster.DevOpenHelper(this, "ORM.sqlite", null);
+        calendarDB = calendarDBHelper.getWritableDatabase();
+
+        // Get DaoMaster
+        daoMaster = new DaoMaster(calendarDB);
+
+        // Create initial database table if they do not exist
+        DaoMaster.createAllTables(calendarDB, true);
+
+        // Create a database access session
+        daoSession = daoMaster.newSession();
+
+        // Get instance of eventDao
+        eventDao = daoSession.getEventDao();
+    }
+
+    private void closeDatabase() {
+        daoSession.clear();
+        calendarDB.close();
+        calendarDBHelper.close();
+    }
+
+    private void closeReopenDatabase() {
+        closeDatabase();
+
+        calendarDBHelper = new DaoMaster.DevOpenHelper(this, "ORM.sqlite", null);
+        calendarDB = calendarDBHelper.getWritableDatabase();
+
+        //Get DaoMaster
+        daoMaster = new DaoMaster(calendarDB);
+
+        // Create DaoSession instance
+        // Use method in DaoMaster to create a database access session
+        daoSession = daoMaster.newSession();
+
+        // From DaoSession instance, get instance of eventDao
+        eventDao = daoSession.getEventDao();
     }
 }
